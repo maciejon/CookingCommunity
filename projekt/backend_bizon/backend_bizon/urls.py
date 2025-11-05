a59cf8fbda82657f5e6a3be1814c985b7e38d9f7
Jaian400 <91557607+Jaian400@users.noreply.github.com>
2025-11-05 18:08:38 +0100

    names_index

--- projekt/backend_bizon/backend_bizon/urls.py
+++ projekt/backend_bizon/backend_bizon/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from main.views import *
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('hello/', hello_world, name='hello-world'),
 ]

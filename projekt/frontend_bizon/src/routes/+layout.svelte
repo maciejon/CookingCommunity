3638f264443a20ec2b646a119837a5f1c92d8c47
maciejon <maciek.olpinski@gmail.com>
2025-11-07 09:20:27 +0100

    podstawka pod wszystko oraz logo

---
+++ projekt/frontend_bizon/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import '../styles/global.css';
+  import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+
+<svelte:head>
+  <title>mam smaka na ptaka</title>
+</svelte:head>
+
+<div class="app-container">
+  <Navbar />
+  <main>
+    <slot />
+  </main>
+
+  <footer>
+    <p>&copy; {new Date().getFullYear()} mam smaka na ptaka. Wszelkie prawa zastrzeżone.</p>
+  </footer>
+</div>
+
+<style>
+
+</style>

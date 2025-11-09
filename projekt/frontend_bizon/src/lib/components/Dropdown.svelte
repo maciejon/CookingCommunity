d7e8da53e1f0a6de624053b11ba1213392b85b2b
Maciej Olpiński <132942969+maciejon@users.noreply.github.com>
2025-11-09 12:53:30 +0100

    dropdown szkielet

---
+++ projekt/frontend_bizon/src/lib/components/Dropdown.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let dropdown_title;
+    /* @type {{etykieta: string, wartosc: string} */
+    export let dropdown_content = [];
+</script>
+
+<div class="dropdown">
+    {dropdown_title}
+    <div class="dropdown-content">
+        {#each dropdown_content as dropdown_option}
+            {dropdown_option}
+        {/each}
+    </div>
+</div> 
+
+<style>
+.dropdown-content {
+  display: none;
+  position: absolute;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
--- projekt/frontend_bizon/src/lib/components/Navbar.svelte
+++ projekt/frontend_bizon/src/lib/components/Navbar.svelte
@@ -1,14 +1,21 @@
+<script>
+    import Dropdown from "./Dropdown.svelte";
+</script>
 <header> <!-- nagłówek strony  -->
   <img class="main_logo" src="/main_logo.png" alt = "mam smaka naptaka">
   <nav> <!--  panel nawigacyjny  -->
-
+  <Dropdown />
   </nav>
 </header>
 
 <style>
   header{
-    background-color: rgb(100,0,0);
+    border-bottom: 1px solid rgb(224,224,224);
     width: 100%;
+  }
+  nav{
+    position:fixed;
+    
   }
   .main_logo{
     width: 10%;

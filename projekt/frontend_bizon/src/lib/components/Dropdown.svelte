7291a85bb035104dacb83df906b71fdaafe6592b
maciejon <maciek.olpinski@gmail.com>
2025-11-11 22:28:45 +0100

    dropdown w pełni działa

--- projekt/frontend_bizon/src/lib/components/Dropdown.svelte
+++ projekt/frontend_bizon/src/lib/components/Dropdown.svelte
@@ -3,35 +3,68 @@
 */ -->
 <script>
     export let title;
+    /*@type {boolean} */
+    export let is_child = false;
     // np. w panelu navbar wartosc to bedzie url
-    /* @type {{etykieta: string, wartosc: string} */ 
+    /* @type {{etykieta: string, wartosc?: string, children?: any[]}[]}  */ 
     export let content = [];
 </script>
 
-<div class="dropdown">
+<div class="dropdown" class:is-child={is_child} >
+  {#if content && content.length > 0}
     {title}
     <div class="dropdown-content">
         {#each content as dropdown_option}
-          <div class="dropdown-option">
+        <div class="dropdown-option">
+             {#if dropdown_option.children}
+             <div class="dropdown-child">
+             <svelte:self title={dropdown_option.etykieta} content={dropdown_option.children} is_child={true} />
+             </div>
+             {:else}
+             <div class="no_children">
             {dropdown_option.etykieta}
-            <br>
+            </div>
+            {/if}  
           </div>
         {/each}
     </div>
-</div> 
+    {/if}
+</div>
 
 <style>
+.dropdown{
+  position: relative;
+  padding: 5px;
+}
+
+.no_children{
+  padding:5px;
+}
+
 .dropdown-content {
   display: none;
   position: absolute;
+  /* border: 1px solid rgb(224,224,224); */
+    border: 1px solid rgb(0,0,0);
 }
 
-.dropdown:hover .dropdown-content {
-  display: block;
+.dropdown:hover > .dropdown-content { /* strzałka > sprawia że działa to tylko na dziecko 1 rzędu*/
+  display:block;
 }
 
 .dropdown-option {
-    margin: 1px;
-    border: 1px solid rgb(224,224,224);
+  color: black;
+  /* padding: 5px; */
+
+}
+
+.dropdown-option:hover{
+  color: white;
+  background-color: black;
+}
+
+.dropdown.is-child > .dropdown-content {
+  left: 100%; 
+  top: 0; 
 }
 </style>
--- projekt/frontend_bizon/src/lib/components/Navbar.svelte
+++ projekt/frontend_bizon/src/lib/components/Navbar.svelte
@@ -2,15 +2,81 @@
     import Dropdown from "./Dropdown.svelte";
 </script>
 <header> <!-- nagłówek strony  -->
-  <img class="main_logo" src="/main_logo.png" alt = "mam smaka naptaka">
+  <div class="highest-div">
+    
   <nav> <!--  panel nawigacyjny  -->
+    <div class="category-buttons">
   <Dropdown
-    title="mam"
-    content = {[
-      {etykieta: "mam", wartosc: "1"},
-      {etykieta: "smaka", wartosc : "2"}
-    ]} />
+    title="Jedzenie"
+    content={[
+      {
+        etykieta: "Śniadanie",
+        children: [
+          {etykieta: "Jajecznica", wartosc: "/sniadanie/jajecznica"},
+          {etykieta: "Tosty", children: [
+            {etykieta: "Z serem i szynką", wartosc: "/sniadanie/tosty/ser-szynka"},
+            {etykieta: "Z awokado", wartosc: "/sniadanie/tosty/awokado"}
+          ]},
+          {etykieta: "Owsianka", wartosc: "/sniadanie/owsianka"}
+        ]
+      },
+      {
+        etykieta: "Obiad",
+        children: [
+          {etykieta: "Zupy", children: [
+            {etykieta: "Rosół", wartosc: "/obiad/zupy/rosol"},
+            {etykieta: "Pomidorowa", wartosc: "/obiad/zupy/pomidorowa"}
+          ]},
+          {etykieta: "Danie główne", children: [
+            {etykieta: "Kotlet schabowy", wartosc: "/obiad/danie-glowne/schabowy"},
+            {etykieta: "Pierogi", children: [
+              {etykieta: "Ruskie", wartosc: "/obiad/danie-glowne/pierogi/ruskie"},
+              {etykieta: "Z mięsem", wartosc: "/obiad/danie-glowne/pierogi/mieso"}
+            ]},
+            {etykieta: "Spaghetti", wartosc: "/obiad/danie-glowne/spaghetti"}
+          ]}
+        ]
+      },
+      {
+        etykieta: "Kolacja",
+        children: [
+          {etykieta: "Kanapki", wartosc: "/kolacja/kanapki"},
+          {etykieta: "Sałatka grecka", wartosc: "/kolacja/salatka-grecka"}
+        ]
+      },
+      {
+        etykieta: "Napoje",
+        children: [
+          {etykieta: "Gorące", children: [
+            {etykieta: "Kawa", wartosc: "/napoje/gorace/kawa"},
+            {etykieta: "Herbata", wartosc: "/napoje/gorace/herbata"}
+          ]},
+          {etykieta: "Zimne", children: [
+            {etykieta: "Woda", wartosc: "/napoje/zimne/woda"},
+            {etykieta: "Sok pomarańczowy", wartosc: "/napoje/zimne/sok"}
+          ]}
+        ]
+      },
+      {
+        etykieta: "Desery",
+        children: [
+          {etykieta: "Ciasta", children: [
+            {etykieta: "Sernik", wartosc: "/desery/ciasta/sernik"},
+            {etykieta: "Szarlotka", wartosc: "/desery/ciasta/szarlotka"}
+          ]},
+          {etykieta: "Lody", wartosc: "/desery/lody"}
+        ]
+      }
+    ]}
+  />
+    </div>
   </nav>
+  </div>
+  <div class="second-div">
+    
+    <img class="main_logo" src="/main_logo.png" alt = "mam smaka naptaka">
+
+  </div>
 </header>
 
 <style>
@@ -19,10 +85,20 @@
     width: 100%;
   }
   nav{
-    position:fixed;
+    position:relative;
     
   }
   .main_logo{
     width: 10%;
+  }
+  .highest-div{
+    background-color: white;
+    position:relative;
+  }
+  .category-buttons{
+    background-color: white;
+    color:black;
+    display: flex;
+    justify-content: center;
   }
 </style>

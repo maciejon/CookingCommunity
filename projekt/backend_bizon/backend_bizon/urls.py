e97bb187b82466d627975c7affb35353e4fc1a63
Jaian400 <91557607+Jaian400@users.noreply.github.com>
2026-01-18 13:35:31 +0100

    upload_przepisu_??

--- projekt/backend_bizon/backend_bizon/urls.py
+++ projekt/backend_bizon/backend_bizon/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('create_review/', create_review, name='create_review'),
     path('update_review/', update_review, name='update_review'),
 
-    path('recipe/upload/', recipe_upload, name='recipe-upload')
+    path('recipe/upload/', RecipeManageView.as_view(), name='recipe-upload')
 ]
--- projekt/image_bizon/main.go
+++ projekt/image_bizon/main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
@@ -20,6 +21,15 @@ func main() {
 
 	rout := chi.NewRouter()
 
+	rout.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173", "http://localhost:8000"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "Upload-Token", "API-Key"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
 	rout.Use(middleware.Logger)
 
 	rout.Get("/{imageName}", imageHandler.ServeImage)
@@ -27,8 +37,12 @@ func main() {
 	// chronione, wymagaja podania api key
 	rout.Group(func(rout chi.Router) {
 		rout.Use(handler.AuthMiddleware(cfg.APIKey))
-		rout.Post("/upload", imageHandler.UploadImage)
 		rout.Delete("/{imageName}", imageHandler.DeleteImage)
+	})
+
+	rout.Group(func(rout chi.Router) {
+		rout.Use(handler.ValidateUploadToken(cfg.APIKey))
+		rout.Post("/upload", imageHandler.UploadImage)
 	})
 
 	addr := fmt.Sprintf(":%s", cfg.Port)
